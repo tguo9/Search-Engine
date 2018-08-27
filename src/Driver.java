76814e6315fe712b3f34239489e4f3b18de14cb8
Sophie Engle <sjengle@cs.usfca.edu>
2018-08-27 16:24:12 -0700

    Initializing project template repository.

---
+++ src/Driver.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+/**
+ * TODO Fill in your own comments!
+ */
+public class Driver {
+
+	/**
+	 * Parses the command-line arguments to build and use an in-memory search
+	 * engine from files or the web.
+	 *
+	 * @param args the command-line arguments to parse
+	 * @return 0 if everything went well
+	 */
+	public static int main(String[] args) {
+		// TODO Fill in
+		System.out.println(Arrays.toString(args));
+		return 0;
+	}
+
+}
