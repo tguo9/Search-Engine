fa7b3c4ba49fc0740d4445618b377df033022260
tguo9 <tguo9@dons.usfca.edu>
2018-09-10 18:49:13 -0700

    Get the project started

--- src/Driver.java
+++ src/Driver.java
@@ -14,7 +14,11 @@ public class Driver {
 	 */
 	public static void main(String[] args) {
 		// TODO Fill in
-		System.out.println(Arrays.toString(args));
+//		System.out.println(Arrays.toString(args));
+		
+		ArgumentMap am = new ArgumentMap();
+		am.parse(args);
+		
 	}
 
 }
