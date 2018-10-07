c992eaf2f61fe90edc79875965870bb2a0920f5e
tguo9 <tguo9@dons.usfca.edu>
2018-10-07 15:55:06 -0700

    add parser

--- src/QueryParser.java
+++ src/QueryParser.java
@@ -1,14 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.TreeSet;
 
 public class QueryParser {
 	
-	public static void parser() {
+	public static String[] parser(String word) {
 		
+		String[] result = new String[10];
 		
+		return result;
 	}
 	
-	public static void uniquer() {
+	public static TreeSet<String> uniquer(String word) {
 		
+		TreeSet<String> result = new TreeSet<>();
 		
+		Collections.addAll(result, parser(word));
+		
+		return result;
 	}
 	
 	public static void sorter() {
