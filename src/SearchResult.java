9c4bfc5a02942de29e93743c9570f291e702c2e2
tguo9 <tguo9@dons.usfca.edu>
2018-10-08 14:45:05 -0700

    add initial methods for this class

--- src/SearchResult.java
+++ src/SearchResult.java
@@ -2,19 +2,39 @@ import java.nio.file.Path;
 
 public class SearchResult implements Comparable<SearchResult> {
 
-	private Path location; 
-	private int words;
+	private Path path; //where
+	private int position; //word position
+	private int words; //count
 	private int locations; 
-	private int frequency;
+	private int frequency; // frequency
 	
-	public SearchResult(Path location, int words, int locations, int frequency) {
+	public SearchResult(Path path, int position, int frequency) {
 		
 		this.frequency = frequency;
-		this.locations = locations;
+		this.position = position;
 		this.frequency = frequency;
 	}
 	
+	public void addFrequency(int frequency){
+		this.frequency += frequency;
+	}
+
 	
+	public void setPosition(int position){
+		this.position = position;
+	}
+
+	public int getFrequency() {
+		return frequency;
+	}
+
+	public int getPosition(){
+		return position;
+	}
+
+	public Path getPath(){
+		return path;
+	}
 	
 	@Override
 	public int compareTo(SearchResult o) {
