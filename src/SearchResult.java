abf3576794a71426d59dc60a6861974ac287b480
tguo9 <tguo9@dons.usfca.edu>
2018-10-07 18:58:59 -0700

    add a class

---
+++ src/SearchResult.java
@@ -0,0 +1,18 @@
+
+public class SearchResult implements Comparable<SearchResult> {
+
+	//location, total word count of the location, and number of times the query occurs at that location.
+	private String location;
+	
+	public SearchResult() {
+		
+		
+	}
+	
+	@Override
+	public int compareTo(SearchResult o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
